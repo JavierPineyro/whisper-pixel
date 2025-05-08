4eab08d4772c416d6c56ae573e5e5353fde630f7
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-05-08 20:08:35 -0300

    Add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <h1 className="text-red-500 text-5xl font-extrabold tracking-tight text-black sm:text-[5rem] text-red-500">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
@@ -25,7 +25,7 @@ export default function HomePage() {
             target="_blank"
           >
             <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
+            <div className="text-lg text-red-700">
               Learn more about Create T3 App, the libraries it uses, and how to
               deploy it.
             </div>
