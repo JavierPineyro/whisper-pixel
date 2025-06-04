5511f7312361f92526e5c17685783b3e6b7b870b
Javier Piñeyro <dariojavierpineyro@gmail.com>
2025-06-04 12:06:26 -0300

    Refactor gitch options config

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,30 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const glitchOptions = {
+    playMode: "always",
+    optimizeSeo: true,
+    createContainers: true,
+    hideOverflow: false,
+    timing: {
+      duration: 2000,
+    },
+    glitchTimeSpan: {
+      start: 0.5,
+      end: 0.7,
+    },
+    shake: {
+      velocity: 27,
+      amplitudeX: 0.05,
+      amplitudeY: 0.03,
+    },
+    slice: {
+      count: 9,
+      velocity: 37,
+      minHeight: 0.02,
+      maxHeight: 0.15,
+      hueRotate: true,
+    },
+    pulse: false,
+  } as const
